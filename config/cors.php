02a890572ced67840ccb09c7d309cc55b4e73448
IPKIN9 <irwandirini@gmail.com>
2023-06-19 12:39:43 +0800

    Modify cors 9

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
   'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
   'allowed_origins' => ['*'],
   'allowed_origins_patterns' => [],
-  'allowed_headers' => ['*'],
+  'allowed_headers' => ['X-PINGOTHER', 'Content-Type', '*'],
   'exposed_headers' => [],
   'max_age' => 86400,
   'supports_credentials' => true,
