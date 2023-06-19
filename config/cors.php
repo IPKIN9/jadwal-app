5a8e276cef5f4124a105457e59d9c29d2b8c1f9a
IPKIN9 <irwandirini@gmail.com>
2023-06-19 11:24:08 +0800

    Modify cors origin 2

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,11 @@
 <?php
 return [
-  'paths'                    => ['*', '*'],
-  'allowed_methods'          => ['POST', 'GET', 'DELETE', 'PUT', '*'],
-  'allowed_origins'          => ['*'],
+  'paths' => ['*'],
+  'allowed_methods' => ['*'],
+  'allowed_origins' => ['*'],
   'allowed_origins_patterns' => [],
-  'allowed_headers'          => ['*'],
-  'exposed_headers'          => [],
-  'max_age'                  => 0,
-  'supports_credentials'     => false,
+  'allowed_headers' => ['*'],
+  'exposed_headers' => [],
+  'max_age' => 0,
+  'supports_credentials' => false,
 ];
