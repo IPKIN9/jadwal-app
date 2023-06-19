47e13f262c8dede97590824096379604d8cde7b1
IPKIN9 <irwandirini@gmail.com>
2023-06-19 12:07:54 +0800

    Modify cors 7

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,11 @@
 <?php
 return [
   'paths' => ['*'],
-  'allowed_methods' => ['*'],
-  'allowed_origins' => ['*'],
+  'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
+  'allowed_origins' => ['*.jadwalpintarsmkn2palu.*'],
   'allowed_origins_patterns' => [],
   'allowed_headers' => ['*'],
   'exposed_headers' => [],
   'max_age' => 86400,
-  'supports_credentials' => false,
+  'supports_credentials' => true,
 ];
