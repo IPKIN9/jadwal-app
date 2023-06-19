ad87dc30f2b7d2e6517fac0c54b16c23abd41957
IPKIN9 <irwandirini@gmail.com>
2023-06-19 11:36:41 +0800

    Modify cors 3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -83,6 +83,7 @@ $app->routeMiddleware([
     'auth' => App\Http\Middleware\Authenticate::class,
     'scopes' => App\Http\Middleware\CostumScopes::class,
     'scope' => App\Http\Middleware\CostumScopesForAny::class,
+    'cors' => Fruitcake\Cors\HandleCors::class,
     // 'scopes' => \Laravel\Passport\Http\Middleware\CheckScopes::class,
     // 'scope' => \Laravel\Passport\Http\Middleware\CheckForAnyScope::class,
 ]);
