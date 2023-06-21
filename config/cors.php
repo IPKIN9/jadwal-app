5daca386fc4516915fe0f0e309d1799c8ebeecd8
IPKIN9 <irwandirini@gmail.com>
2023-06-21 17:23:07 +0800

    New cors setup

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,11 @@
 <?php
 return [
   'paths' => ['*'],
-  'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
+  'allowed_methods' => ['*'],
   'allowed_origins' => ['*'],
   'allowed_origins_patterns' => [],
-  'allowed_headers' => ['X-PINGOTHER', 'Content-Type', '*'],
+  'allowed_headers' => ['*'],
   'exposed_headers' => [],
-  'max_age' => 86400,
-  'supports_credentials' => true,
+  'max_age' => 0,
+  'supports_credentials' => false,
 ];
